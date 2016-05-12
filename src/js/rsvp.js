20dd48b7e93b26ac86518e640e488d8fe0ac88ce
Helen Durrant <hh.durrant@gmail.com>
2016-05-12 20:00:33 +1200

    End to end form stuff. Yay!

---
+++ src/js/rsvp.js
@@ -0,0 +1,69 @@
+/*global $*/
+$(function() {
+    'use strict';
+
+    $('#contactForm input, #contactForm textarea').jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+            console.log(errors);
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault();
+
+            var data = {
+                names: $('#names').val(),
+                bus: $('.bus[checked]').attr('name'),
+                email: $('#email').val(),
+                extraInfo: $('#extraInfo').val()
+            };
+
+            $.ajax({
+                url: '/api/people',
+                type: 'POST',
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+                data: JSON.stringify(data),
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#success').html('<div class="alert alert-success">');
+                    $('#success > .alert-success')
+                        .html('<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>');
+                    $('#success > .alert-success').append('<strong>Your message has been sent. </strong>');
+                    $('#success > .alert-success').append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger('reset');
+                },
+                error: function() {
+                    // Fail message
+                    $('#success')
+                        .html('<div class="alert alert-danger">');
+                    $('#success > .alert-danger')
+                        .html('<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>');
+                    $('#success > .alert-danger')
+                        .append('<strong>Sorry it seems that my mail server is not responding. Please try again later!');
+                    $('#success > .alert-danger').append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger('reset');
+                }
+            });
+        },
+        filter: function() {
+            return $(this).is(':visible');
+        }
+    });
+
+    $('a[data-toggle=\"tab\"]').click(function(e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+
+    /*When clicking on Full hide fail/success boxes */
+    $('#names').focus(function() {
+        $('#success').html('');
+    });
+});
+
--- src/server.js
+++ src/server.js
@@ -1,24 +1,26 @@
+/*eslint strict: ["error", "global"]*/
+/* eslint-disable no-console */
 'use strict';
 
 var bodyParser = require('body-parser'),
-	express = require('express'),
-	MongoClient = require('mongodb').MongoClient,
+    express = require('express'),
+    MongoClient = require('mongodb').MongoClient,
 
-	// Connection URL
-	url = 'mongodb://localhost:27017/wedding',
+    // Connection URL
+    url = 'mongodb://localhost:27017/wedding',
 
-	// The app
-	app = express();
+    // The app
+    app = express();
 
 function insertPerson(db, person, callback) {
-	// ALL THE PEOPLE?!
-	var collection = db.collection('people');
+    // ALL THE PEOPLE?!
+    var collection = db.collection('people');
 
-	// Have one more!
-	collection.insertOne(person, function(err, result) {
-		// FIXME handle this error
-		callback(result);
-	});
+    // Have one more!
+    collection.insertOne(person, function(err, result) {
+        // FIXME handle this error
+        callback(result);
+    });
 }
 
 // Serve static files
@@ -27,28 +29,30 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
 // Serve index.html on root
-app.get('/', function (req, res) {
-  res.redirect('./index.html');
+app.get('/', function(req, res) {
+    res.redirect('./index.html');
 });
 
 // Serve app on port 3000
-app.listen(3000, function () {
-	console.log('App listening on port 3000!');
+app.listen(3000, function() {
+    console.log('App listening on port 3000!');
 });
 
-app.post('/api/people', function (req, res) {
+app.post('/api/people', function(req, res) {
 
-	// Connect to mongo
-	MongoClient.connect(url, function(err, db) {
+    console.log(req.body);
 
-		// Couldn't connect to DB
-		if (err) {
-  			res.status(500).send({message: 'Can\'t connect to the DB :('});
-		} else {
-			insertPerson(db, req.body, function() {
-				db.close();
-				res.status(200).send();
-			});
-		}
-	});
+    // Connect to mongo
+    MongoClient.connect(url, function(err, db) {
+
+        // Couldn't connect to DB
+        if (err) {
+            res.status(500).send({message: 'Can\'t connect to the DB :('});
+        } else {
+            insertPerson(db, req.body, function() {
+                db.close();
+                res.status(200).send({});
+            });
+        }
+    });
 });
