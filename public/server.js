aad7720b08e28dd28010330c287d30e2471b8528
Lisa Quayle <itsthelisa@gmail.com>
2016-05-14 13:58:56 +1200

    Committed updating built files

--- public/server.js
+++ public/server.js
@@ -37,7 +37,7 @@ app.get('/', function(req, res) {
 });
 
 // Serve app on port 3000
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('App listening on port 3000!');
 });
 
