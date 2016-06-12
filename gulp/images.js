bdf1f864dae1da23ec0e2c881ffcfe076bb6c7f4
Lisa Quayle <itsthelisa@gmail.com>
2016-06-12 13:37:09 +1200

    Minified images.

--- gulp/images.js
+++ gulp/images.js
@@ -2,11 +2,12 @@
 
 var changed = require('gulp-changed'),
     gulp = require('gulp'),
-
+    imagemin = require('gulp-imagemin'),
     dest = './public/img';
 
 gulp.task('images', function() {
     return gulp.src('src/img/**/*')
+        .pipe(imagemin())
         .pipe(changed(dest))
         .pipe(gulp.dest(dest));
 });
